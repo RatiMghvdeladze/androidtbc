90bf8827a32bc6646ce5431ebc45d00edd28a2dc
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2024-12-19 18:50:44 +0400

    pasfakfasklfmkkkk

---
+++ app/src/main/java/com/example/androidtbc/LogInFragment.kt
@@ -0,0 +1,45 @@
+package com.example.androidtbc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidtbc.databinding.FragmentLogInBinding
+import com.example.androidtbc.databinding.FragmentSignInBinding
+
+class LogInFragment : Fragment() {
+    private var _binding: FragmentLogInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLogInBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       init()
+    }
+
+    private fun init(){
+        binding.btnSignInWithPassword.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.container, SignInFragment()).addToBackStack(null)
+                .commit()
+        }
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/example/androidtbc/SignInFragment.kt
@@ -0,0 +1,41 @@
+package com.example.androidtbc
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.androidtbc.databinding.FragmentSignInBinding
+
+class SignInFragment : Fragment() {
+    private var _binding: FragmentSignInBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+
+    }
+
+    private fun init(){
+        binding.btnBackButton.setOnClickListener{
+            parentFragmentManager.popBackStack()
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
