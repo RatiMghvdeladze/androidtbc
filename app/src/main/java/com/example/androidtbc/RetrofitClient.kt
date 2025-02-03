3960c65873951abe16a76f5b71e23f1ed3b17c98
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2025-02-03 09:01:36 +0400

    nothing just commit

--- app/src/main/java/com/example/androidtbc/RetrofitClient.kt
+++ app/src/main/java/com/example/androidtbc/RetrofitClient.kt
@@ -1,4 +1,23 @@
 package com.example.androidtbc
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
 object RetrofitClient {
+    private const val BASE_URL = "https://run.mocky.io/v3/"
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        explicitNulls = false
+    }
+
+    private fun retrofit(): Retrofit {
+        return Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType())).build()
+    }
+
+
+    val apiService: ApiService = retrofit().create(ApiService::class.java)
 }
