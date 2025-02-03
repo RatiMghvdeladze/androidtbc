8cf4f2b6162579162f7c244dcc11a46a1bfd505c
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2025-02-03 12:06:41 +0400

    shemajamebeli 6

--- app/src/main/java/com/example/androidtbc/data/UserViewModel.kt
+++ app/src/main/java/com/example/androidtbc/data/UserViewModel.kt
@@ -3,18 +3,13 @@ package com.example.androidtbc.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class UserViewModel(private val repository: UserRepository) : ViewModel() {
-    val users = repository.usersFlow.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(3000),
-        initialValue = emptyList()
-    )
+    private val _users = MutableStateFlow<List<UserEntity>>(emptyList())
+    val users: StateFlow<List<UserEntity>> = _users.asStateFlow()
 
     val isOnline = repository.isOnline
 
@@ -25,6 +20,11 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
     val error: StateFlow<String?> = _error.asStateFlow()
 
     init {
+        viewModelScope.launch {
+            repository.usersFlow.collect { users ->
+                _users.value = users
+            }
+        }
         refreshUsers()
     }
 
