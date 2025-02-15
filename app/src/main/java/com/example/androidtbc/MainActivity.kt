69b66e703d4d6fc0a09068e0a8d49291c8cb4e60
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2025-02-15 16:09:26 +0400

    nothing

--- app/src/main/java/com/example/androidtbc/MainActivity.kt
+++ app/src/main/java/com/example/androidtbc/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.androidtbc
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -21,12 +23,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
 
         val navController = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.let {
             (it as? NavHostFragment)?.navController
         } ?: findNavController(R.id.fragmentContainerView)
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        binding.bottomNavigationView.selectedItemId = R.id.mainFragment
     }
 
 }
