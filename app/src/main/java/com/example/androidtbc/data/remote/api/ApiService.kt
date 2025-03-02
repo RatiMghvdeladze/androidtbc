0e383e2ccf97c85a11d55341405f96865c133bc8
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2025-03-02 20:21:44 +0400

    use companion object for well structure

--- app/src/main/java/com/example/androidtbc/data/remote/api/ApiService.kt
+++ app/src/main/java/com/example/androidtbc/data/remote/api/ApiService.kt
@@ -12,53 +12,60 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("movie/popular")
+    @GET(POPULAR_MOVIES)
     suspend fun getPopularMovies(
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("page") page: Int = DEFAULT_PAGE
     ) : Response<PopularMovieDto>
 
-    @GET("movie/now_playing")
+    @GET(NOW_PLAYING_MOVIES)
     suspend fun getNowPlayingMovies(
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("page") page: Int = DEFAULT_PAGE
     ) : Response<NowPlayingMovieDto>
 
-    @GET("movie/top_rated")
+    @GET(TOP_RATED_MOVIES)
     suspend fun getTopRatedMovies(
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("page") page: Int = DEFAULT_PAGE
     ) : Response<TopRatedMovieDto>
 
-
-    @GET("movie/upcoming")
+    @GET(UPCOMING_MOVIES)
     suspend fun getUpcomingMovies(
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("page") page: Int = DEFAULT_PAGE
     ) : Response<UpcomingMovieDto>
 
-
-    @GET("search/movie")
+    @GET(SEARCH_MOVIES)
     suspend fun searchMovies(
         @Query("query") query: String,
-        @Query("language") language: String = "en-US",
-        @Query("page") page: Int = 1
+        @Query("language") language: String = DEFAULT_LANGUAGE,
+        @Query("page") page: Int = DEFAULT_PAGE
     ) : Response<PopularMovieDto>
 
-    @GET("movie/{movie_id}")
+    @GET(MOVIE_DETAILS)
     suspend fun getMovieDetails(
         @Path("movie_id") movieId: Int,
-        @Query("language") language: String = "en-US"
+        @Query("language") language: String = DEFAULT_LANGUAGE
     ): Response<MovieDetailDto>
 
-
-    @GET("movie/{movie_id}/credits")
+    @GET(MOVIE_CREDITS)
     suspend fun getMovieCredits(
         @Path("movie_id") movieId: Int,
-        @Query("language") language: String = "en-US"
+        @Query("language") language: String = DEFAULT_LANGUAGE
     ): Response<CastDto>
 
 
+    companion object {
+        private const val DEFAULT_LANGUAGE = "en-US"
+        private const val DEFAULT_PAGE = 1
 
-
+        private const val POPULAR_MOVIES = "movie/popular"
+        private const val NOW_PLAYING_MOVIES = "movie/now_playing"
+        private const val TOP_RATED_MOVIES = "movie/top_rated"
+        private const val UPCOMING_MOVIES = "movie/upcoming"
+        private const val SEARCH_MOVIES = "search/movie"
+        private const val MOVIE_DETAILS = "movie/{movie_id}"
+        private const val MOVIE_CREDITS = "movie/{movie_id}/credits"
+    }
 }
