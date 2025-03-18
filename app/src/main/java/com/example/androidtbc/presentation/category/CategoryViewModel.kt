b65d713eda269501de796580bf3038a7e97e99aa
RatiMghvdeladze <mghvdeladze.rati@gmail.com>
2025-03-18 21:05:17 +0400

    update viewmodel

--- app/src/main/java/com/example/androidtbc/presentation/category/CategoryViewModel.kt
+++ app/src/main/java/com/example/androidtbc/presentation/category/CategoryViewModel.kt
@@ -6,14 +6,18 @@ import com.example.androidtbc.domain.common.Resource
 import com.example.androidtbc.domain.usecase.GetCategoriesUseCase
 import com.example.androidtbc.presentation.mapper.toPresentationList
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@Suppress("OPT_IN_USAGE")
 @HiltViewModel
 class CategoryViewModel @Inject constructor(
     private val getCategoriesUseCase: GetCategoriesUseCase
@@ -22,21 +26,25 @@ class CategoryViewModel @Inject constructor(
     private val _state = MutableStateFlow(CategoryState())
     val state: StateFlow<CategoryState> = _state.asStateFlow()
 
-    private var searchJob: kotlinx.coroutines.Job? = null
+    private val _searchQuery = MutableStateFlow("")
 
     init {
-        onEvent(CategoryEvent.LoadCategories)
+        _searchQuery
+            .debounce(500)
+            .distinctUntilChanged()
+            .onEach { query ->
+                loadCategories(query)
+            }
+            .launchIn(viewModelScope)
+
+        loadCategories("")
     }
 
     fun onEvent(event: CategoryEvent) {
         when (event) {
             is CategoryEvent.OnSearchQueryChanged -> {
                 _state.update { it.copy(searchQuery = event.query) }
-                searchJob?.cancel()
-                searchJob = viewModelScope.launch {
-                    delay(500)
-                    loadCategories(event.query)
-                }
+                _searchQuery.value = event.query
             }
             is CategoryEvent.LoadCategories -> {
                 loadCategories(_state.value.searchQuery)
@@ -59,7 +67,7 @@ class CategoryViewModel @Inject constructor(
                         }
                     }
                     is Resource.Error -> {
-                         _state.update {
+                        _state.update {
                             it.copy(
                                 isLoading = false,
                                 error = result.errorMessage
@@ -73,5 +81,4 @@ class CategoryViewModel @Inject constructor(
             }
         }
     }
-
 }
